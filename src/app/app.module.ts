b25853fe37490bb4dd99a4e9d2615662476a0575
ByKos92 <bukov92@mail.ru>
2022-04-14 03:45:10 +0600

    add new component RecComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PriceComponent } from './price/price.component';
+import { RecComponent } from './rec/rec.component';
 
 @NgModule({
-  declarations: [AppComponent, PriceComponent],
+  declarations: [AppComponent, PriceComponent, RecComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/rec/rec.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rec',
+  templateUrl: './rec.component.html',
+  styleUrls: ['./rec.component.scss'],
+})
+export class RecComponent {}
