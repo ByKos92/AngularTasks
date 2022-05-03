d5a34fa30a2a0408d24bd2aa7ac9535a05d41aef
ByKos92 <bukov92@mail.ru>
2022-05-03 22:28:08 +0600

    add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PriceComponent } from './price/price.component';
 import { RecComponent } from './rec/rec.component';
 import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'rec', component: RecComponent },
+  { path: 'price', component: PriceComponent },
+];
 @NgModule({
-  declarations: [AppComponent, PriceComponent, RecComponent, TestComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    PriceComponent,
+    RecComponent,
+    TestComponent,
+    HomeComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
 })
